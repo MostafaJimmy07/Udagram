0da585d802fe46754fcf256b5a7662255ab7610e
MostafaJimmy07 <mg8635602@gmail.com>
2022-03-15 14:12:03 +0200

    final commit

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,2 +1,5 @@
+eb setenv POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST DB_PORT=$DB_PORT POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb use udagram-api-dev
 eb deploy udagram-api-dev
+eb status udagram-api-dev
+
